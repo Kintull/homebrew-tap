96f674595817fc77f64c01a2546307712ef693fb
Roman Berdichevskii <r378ut@gmail.com>
2026-04-29 13:39:41 +0200

    Update github-tracker to v1.0.6

--- Formula/github-tracker.rb
+++ Formula/github-tracker.rb
@@ -1,9 +1,9 @@
 class GithubTracker < Formula
   desc "A TUI application for tracking GitHub PRs"
   homepage "https://github.com/Kintull/gihub-pr-poller"
-  url "https://github.com/Kintull/gihub-pr-poller/releases/download/v1.0.5/github-tracker-macos-arm64.tar.gz"
-  sha256 "7be468d9ff6619788f799db83c1dccdd03f1eddfc1c120b943709ccad22245ee"
-  version "1.0.5"
+  url "https://github.com/Kintull/gihub-pr-poller/releases/download/v1.0.6/github-tracker-macos-arm64.tar.gz"
+  sha256 "23999c19e668cdb1cc48223c48959e0d57b87413f02559e0b733f918fd40e5b7"
+  version "1.0.6"
   license "MIT"
 
   def install
